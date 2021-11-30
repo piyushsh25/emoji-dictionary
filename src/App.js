5c9ab992dd2ee028d7a86967cb266bc764af3d93
piyushpoudyal <72690317+piyushpoudyal@users.noreply.github.com>
2021-11-30 11:25:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance"
+};
+var emojis = Object.keys(emojiDictionary);
+
+function App() {
+  const [meaning, setMeaning] = useState("");
+  function emojiHandler(e) {
+    var userInput = e.target.value;
+    var meaning = emojiDictionary[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Not in our DB";
+    }
+    setMeaning(meaning);
+  }
+  function emojiHandler(emojis) {
+    var meaning = emojiDictionary[emojis];
+    setMeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Welcome!</h1>
+      <input onChange={emojiHandler}></input>
+      <h2>{meaning}</h2>
+      <h3>Emojis we know</h3>
+      {emojis.map((emojis) => {
+        return (
+          <span
+            onClick={() => emojiHandler(emojis)}
+            style={{ fontSize: "2rem", margin: "1rem", cursor: "pointer" }}
+            key={emojis}
+          >
+            {emojis}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
+export default App;
